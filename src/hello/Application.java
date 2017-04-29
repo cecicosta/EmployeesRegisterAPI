efce27d8c44e2bed8f243a603917b7d9c34bb0fe
cesar costa <caezar.jc@gmail.com>
2017-04-29 12:01:20 -0300

    Implemented JWT token login checking.

--- src/hello/Application.java
+++ src/hello/Application.java
@@ -2,8 +2,11 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = com.authentication.jwt.LoginController.class)
+@ComponentScan(basePackageClasses = hello.Greeting.class)
 public class Application {
 
     public static void main(String[] args) {
